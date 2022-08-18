481a3a5f47d8f33b422875aba563b654d946fb9f
decktech <mdecker113@gmail.com>
2022-08-18 18:23:10 -0400

    wop

--- data/migrations/20220818220214_initial-migration.js
+++ data/migrations/20220818220214_initial-migration.js
@@ -1,10 +1,16 @@
 exports.up = function (knex) {
     return knex.schema
         .createTable('recipes', tbl => {
-            tbl.increments()
+            tbl.increments('recipe_id')
+            tbl.varchar('recipe_name', 100)
+                .notNullable();
         })
         .createTable('ingredients', tbl => {
-            tbl.increments()
+            tbl.increments('ingredient_id')
+            tbl.varchar('ingredient_name', 100)
+                .notNullable()
+                .unique();
+            tbl.varchar('ingredient_unit', 100)
         })
         .createTable('steps', tbl => {
             tbl.increments()
